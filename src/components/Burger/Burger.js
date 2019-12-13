5b1bde4a236cd6dc3cdd038378dedecc7049c3f6
David Pal <paldavid2030@gmail.com>
2019-12-13 17:36:22 +0300

    create BurgerIngredient component and Burger component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const Burger = (props) => (
+  <BurgerIngredient></BurgerIngredient>
+); 
+
+export default Burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,13 @@
 import React, {Component, Fragment} from 'react';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+
   render() {
     return (
       <Fragment>
-        <div>Burger</div>
+        <Burger />
         <div>Burger controls</div>
       </Fragment>
     )
