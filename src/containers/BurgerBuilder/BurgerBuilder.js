1199c735914dae9f633878810b96368ac7f9d214
David Pal <paldavid2030@gmail.com>
2019-12-17 15:23:56 +0300

    make burger controls dynamic

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,37 +1,47 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BurgerControls from '../../components/BurgerControls/BurgerControls';
 
 class BurgerBuilder extends Component {
 
-constructor(props) {
-  super(props);
+  constructor(props) {
+    super(props);
 
-  this.state = {
-  ingredients: {
-    salad: 1,
-    cheese: 1,
-    bacon: 1,
-    meat: 1,
+    this.state = {
+      ingredients: {
+        salad: 1,
+        cheese: 1,
+        bacon: 1,
+        meat: 1,
+      }
+    }
   }
-}
-}
 
-changeAmount = (lessOrMore, ingredient) => {
-  // if (lessOrMore === 'less') {
-  //   this.setState(prevstate => prevstate.ingredients[ingredient] - 1);
-  // } else if (lessOrMore === 'more') {
-  //   this.setState(prevstate => prevstate.ingredients[ingredient] + 1);
-  // }
-
-  console.log(lessOrMore, ingredient);
-}
+  changeAmount = (lessOrMore, ingredient) => {
+    if (lessOrMore === 'less') {
+      this.setState(prevState => ({
+        ingredients: {
+          ...prevState.ingredients,
+          [ingredient]: prevState.ingredients[ingredient] - 1,
+        }
+      })
+      );
+    } else if (lessOrMore === 'more') {
+      this.setState(prevState => ({
+        ingredients: {
+          ...prevState.ingredients,
+          [ingredient]: prevState.ingredients[ingredient] + 1,
+        }
+      })
+      );
+    }
+  }
 
   render() {
     return (
       <Fragment>
         <Burger ingredients={this.state.ingredients} />
-        <BurgerControls ingredients={this.state.ingredients} changeAmount={this.changeAmount}/>
+        <BurgerControls ingredients={this.state.ingredients} changeAmount={this.changeAmount} />
       </Fragment>
     )
   }
