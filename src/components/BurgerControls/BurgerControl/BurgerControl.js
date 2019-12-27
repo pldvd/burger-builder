c9ddaebd11944f050404718ef0b01804eedf0d98
David Pal <paldavid2030@gmail.com>
2019-12-27 23:48:10 +0300

    style paragraphs in BurgerControl

--- src/components/BurgerControls/BurgerControl/BurgerControl.js
+++ src/components/BurgerControls/BurgerControl/BurgerControl.js
@@ -5,8 +5,8 @@ const BurgerControl = (props) => {
 
   return (
     <div className={styles.BurgerControl}>
-      <p>{props.name}</p>
-      <p>{props.amount}</p>
+      <p className={styles.text}>{props.name}</p>
+      <p className={styles.text}>{props.amount}</p>
       <button className={styles['btn--less']} onClick={() => props.changeAmount('less', props.name)} disabled={props.amount > 0 ? false : true}>Less</button>
       <button className={styles['btn--more']} onClick={() =>  props.changeAmount('more', props.name)}>More</button>
     </div>
