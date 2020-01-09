5e33131e84bc6215edb1d02c0ffcee4581f4f0fb
David Pal <paldavid2030@gmail.com>
2020-01-09 12:50:25 +0300

    add Navigation component

---
+++ src/components/Toolbar/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Navigation.module.scss';
+
+const Navigation = () => (
+  <ul className={styles.Navigation}>
+    <li>Hello</li>
+    <li>Bello</li>
+  </ul>
+);
+
+export default Navigation;
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './Toolbar.module.scss';
+import Navigation from './Navigation/Navigation';
 import logoImage from '../../../src/burger-logo.png';
 
 const Toolbar = (props) => {
   return (
     <header className={styles.Toolbar}>
       <img src={logoImage} alt={'Burger logo'}/>
-      <nav>NavList</nav>
+      <Navigation />
       <div>SideDrawer</div>
     </header>
   )
