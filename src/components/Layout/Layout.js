1d1b13a7c60866f63ae76c9b0c5bf4d62439c2a9
David Pal <paldavid2030@gmail.com>
2020-01-10 14:31:07 +0300

    add display Backdrop component is sidedrawer is open

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from 'react';
 import Toolbar from '../Toolbar/Toolbar';
 import styles from './Layout.module.css';
 import SideDrawer from '../SideDrawer/SideDrawer';
+import Backdrop from '../UI/Backdrop/Backdrop';
 
 const Layout = (props) => {
 
@@ -14,6 +15,7 @@ const Layout = (props) => {
   return (
     <Fragment>
       <Toolbar sideControl={toggleOpen} />
+      <Backdrop show={sideIsOpen}/>
       <SideDrawer isOpen={sideIsOpen} sideControl={toggleOpen}/>
       <main className={styles.Content}>{props.children}</main>
     </Fragment>
