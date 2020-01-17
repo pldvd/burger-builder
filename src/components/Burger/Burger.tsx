3ba16b18426bf201cf21f485d13db934b72b5ae1
David Pal <paldavid2030@gmail.com>
2020-01-17 17:38:15 +0300

    convert js files into tsx

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import styles from './Burger.module.scss';
-import PropTypes from 'prop-types';
 
-const Burger = (props) => {
+
+interface BurgerProps {
+  ingredients: {
+    salad: number,
+    cheese: number,
+    bacon: number,
+    meat: number,
+    [index: string] : number
+  }
+}
+
+const Burger: React.FC<BurgerProps> = (props) => {
 
   const ingreds = Object.keys(props.ingredients).flatMap(key => {
     return Array.from({ length: props.ingredients[key] }, () => key);
@@ -12,16 +22,12 @@ const Burger = (props) => {
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type='bread-top' />
-      { ingreds.length === 0 ? <p className={styles.p}>Please add ingredients</p> :
+      {ingreds.length === 0 ? <p className={styles.p}>Please add ingredients</p> :
         ingreds.map((ingr, index) => <BurgerIngredient type={ingr} key={index} />)
       }
       <BurgerIngredient type='bread-bottom' />
     </div>
   );
-}
-
-Burger.propTypes = {
-  ingredients: PropTypes.object
 }
 
 export default Burger;
