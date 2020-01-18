fc4808dfc1af8ff8b85a3ab6f28b5e02f3834672
David Pal <paldavid2030@gmail.com>
2020-01-18 20:52:01 +0300

    fix type checking compile errors in HamburgerIcon

--- src/components/UI/HamburgerIcon/HamburgerIcon.tsx
+++ src/components/UI/HamburgerIcon/HamburgerIcon.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import styles from './HamburgerIcon.module.scss'
 
-const HamburgerIcon = (props) => (
+interface HamburgerIconProps {
+  sideControl: () => void
+}
+
+const HamburgerIcon: React.FC<HamburgerIconProps> = (props) => (
   <svg height="32px"
     viewBox="0 0 32 32"
     width="32px"
-    space="preserve"
+    // space="preserve"
     xmlns="http://www.w3.org/2000/svg"
-    xlink="http://www.w3.org/1999/xlink"
+    // xlink="http://www.w3.org/1999/xlink"
     className={styles.HamburgerIcon}
     onClick={props.sideControl}>
     <path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z" />
