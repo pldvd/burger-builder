fb0b038befbe801a955b55763f8232b0e36eeae8
David Pal <paldavid2030@gmail.com>
2020-01-24 13:31:35 +0300

    style the CheckoutSummary component

--- src/components/Order/CheckoutSummary/CheckoutSummary.tsx
+++ src/components/Order/CheckoutSummary/CheckoutSummary.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import Burger,{BurgerProps} from '../../Burger/Burger';
+import Burger, { BurgerProps } from '../../Burger/Burger';
 import Button from '../../UI/Button/Button';
+import styles from './CheckoutSummary.module.scss';
 
-interface CheckoutSummaryProps extends BurgerProps{
+interface CheckoutSummaryProps extends BurgerProps {
   continue: () => void,
   cancel: () => void,
 }
 
 const CheckoutSummary: React.FC<CheckoutSummaryProps> = (props) => {
   return (
-    <div>
-      <Burger ingredients={props.ingredients}/>
-      <Button color="green" clicked={props.continue}>Continue</Button>
-      <Button color="red" clicked={props.cancel}>Cancel</Button>
+    <div className={styles.CheckoutSummary}>
+      <h1 style={{textAlign: "center"}}>We hope you will like your burger!</h1>
+      <div className={styles.Burger}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <div className={styles.CenterButtons}>
+        <Button color="green" clicked={props.continue}>Continue</Button>
+        <Button color="red" clicked={props.cancel}>Cancel</Button>
+      </div>
     </div>
   )
 }
--- src/containers/Checkout.tsx
+++ src/containers/Checkout.tsx
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import CheckoutSummary from '../components/Order/CheckoutSummary/CheckoutSummary';
 
-export default class Checkout extends Component {
+class Checkout extends Component {
+
+  state = {
+    ingredients: {
+      salad:1,
+      cheese: 1,
+      bacon: 1,
+      meat: 1,
+    }
+  }
+
   render() {
     return (
       <div>
-        
+        <CheckoutSummary ingredients={this.state.ingredients} cancel={() => 'hello'} continue={() => 'hello'}/>
       </div>
     )
   }
 }
+
+export default Checkout;
