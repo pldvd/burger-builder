5acdc00e74a2d2b397fe778e33c07a3306112718
David Pal <paldavid2030@gmail.com>
2020-01-31 14:57:57 +0300

    style OrderCell component

---
+++ src/components/Order/OrderCell/OrderCell.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import style from './OrderCell.module.scss';
+
+const OrderCell = (props) => {
+
+  const ingreds = Object.keys(props.ingredients).map(key => (
+    <div className={style.ingred}>
+      <span>{key}</span>
+      <span>({props.ingredients[key]})</span>
+    </div>
+  ))
+
+  return (
+    <div className={style.OrderCell}>
+      <h4>Ingredients:</h4>
+      <div className={style.ingredsList}>
+        {ingreds}
+      </div>
+      <p><span className={style.price}>Price: </span><span>USD {props.price}</span></p>
+    </div>
+  )
+}
+
+export default OrderCell;
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import OrderCell from '../../components/Order/OrderCell/OrderCell';
 
 class Orders extends Component {
+
+  state = {
+    ingredients: {
+      bacon: 0,
+      meat: 1,
+      salad: 2,
+      cheese: 1
+    }
+  }
+
   render() {
-    return (
-      <div>
-        CSOMI
-      </div>
-    )
+    return <OrderCell price='8.00' ingredients={this.state.ingredients}/>
   }
 }
 
