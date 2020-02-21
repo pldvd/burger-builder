2e93c47c0a618e8fef01d7a84b1188c602eb9e17
David Pal <paldavid2030@gmail.com>
2020-02-21 10:33:51 +0300

    refactor CheckoutForm component: extract types to separate file

--- src/containers/Checkout/CheckoutForm/CheckoutForm.tsx
+++ src/containers/Checkout/CheckoutForm/CheckoutForm.tsx
@@ -1,41 +1,12 @@
 import React, { Component } from 'react';
-import { RouteComponentProps, withRouter } from 'react-router';
+import { withRouter } from 'react-router';
 import Button from '../../../components/UI/Button/Button';
 import Input from '../../../components/UI/Input/Input';
 import Loader from '../../../components/UI/Loader/Loader';
 import styles from './CheckoutForm.module.scss';
-import { IngredientType } from '../../../components/Burger/Burger';
 import Axios from '../../../axios';
+import { orderDataInterface, CheckoutFormInterface, CheckoutFormProps } from './types';
 
-interface orderDataInterface {
-  name: string,
-  nameIsValid: boolean,
-  email: string,
-  emailIsValid: boolean,
-  street: string,
-  streetIsValid: boolean,
-  postalCode: string,
-  postalCodeIsValid: boolean,
-  country: string,
-  countryIsValid: boolean,
-  deliveryMethod: string,
-  deliveryMethodIsValid: boolean,
-  [i: string]: string | boolean
-}
-
-interface CheckoutFormInterface {
-  orderData: orderDataInterface,
-  isLoading: boolean,
-  formIsValid: boolean,
-  formIsTouched: boolean,
-  touchedFields: string[],
-  [i: string]: string | boolean | orderDataInterface | string[]
-}
-
-interface CheckoutFormProps extends RouteComponentProps {
-  ingredients: IngredientType,
-  finalPrice: number | null
-}
 
 class CheckoutForm extends Component<CheckoutFormProps, CheckoutFormInterface> {
 
@@ -148,11 +119,11 @@ class CheckoutForm extends Component<CheckoutFormProps, CheckoutFormInterface> {
       this.setState({ formIsTouched: true });
     }
     const isFormValid = (state: CheckoutFormInterface): boolean => {
-      return(state.formIsTouched && state.orderData.nameIsValid && state.orderData.emailIsValid && state.orderData.countryIsValid && state.orderData.deliveryMethodIsValid && state.orderData.postalCodeIsValid);
+      return (state.formIsTouched && state.orderData.nameIsValid && state.orderData.emailIsValid && state.orderData.countryIsValid && state.orderData.deliveryMethodIsValid && state.orderData.postalCodeIsValid);
     }
 
     this.setState({ orderData: orderDataCopy, touchedFields: touchedFieldsCopy }, () => {
-      this.setState({formIsValid: isFormValid(this.state)}) //need to validate form once orderData state is surely updated, hence the setState in callback
+      this.setState({ formIsValid: isFormValid(this.state) }) //need to validate form once orderData state is surely updated, hence the setState in callback
     });
   }
 
@@ -190,7 +161,7 @@ class CheckoutForm extends Component<CheckoutFormProps, CheckoutFormInterface> {
             onChange={this.handleInputChange}
             isValid={this.state.orderData.postalCodeIsValid}
             touchedFields={this.state.touchedFields}
-            />
+          />
           <Input
             inputtype="input"
             type="text"
@@ -200,7 +171,7 @@ class CheckoutForm extends Component<CheckoutFormProps, CheckoutFormInterface> {
             onChange={this.handleInputChange}
             isValid={this.state.orderData.countryIsValid}
             touchedFields={this.state.touchedFields}
-            />
+          />
           <Input
             inputtype="input"
             type="email"
@@ -210,12 +181,12 @@ class CheckoutForm extends Component<CheckoutFormProps, CheckoutFormInterface> {
             onChange={this.handleInputChange}
             isValid={this.state.orderData.emailIsValid}
             touchedFields={this.state.touchedFields}
-            />
+          />
           <Input
             inputtype="select"
             name="delivery-method"
             id="delivery-method"
-            displayvalues={['Please select...','Fastest', 'Cheapest']}
+            displayvalues={['Please select...', 'Fastest', 'Cheapest']}
             onChange={this.handleInputChange}
             isValid={this.state.orderData.deliveryMethodIsValid}
             touchedFields={this.state.touchedFields}
---
+++ src/containers/Checkout/CheckoutForm/types.ts
@@ -0,0 +1,33 @@
+import { RouteComponentProps } from 'react-router-dom';
+import { IngredientType } from '../../../components/Burger/Burger';
+
+
+export interface orderDataInterface {
+  name: string,
+  nameIsValid: boolean,
+  email: string,
+  emailIsValid: boolean,
+  street: string,
+  streetIsValid: boolean,
+  postalCode: string,
+  postalCodeIsValid: boolean,
+  country: string,
+  countryIsValid: boolean,
+  deliveryMethod: string,
+  deliveryMethodIsValid: boolean,
+  [i: string]: string | boolean
+}
+
+export interface CheckoutFormInterface {
+  orderData: orderDataInterface,
+  isLoading: boolean,
+  formIsValid: boolean,
+  formIsTouched: boolean,
+  touchedFields: string[],
+  [i: string]: string | boolean | orderDataInterface | string[]
+}
+
+export interface CheckoutFormProps extends RouteComponentProps {
+  ingredients: IngredientType,
+  finalPrice: number | null
+}
