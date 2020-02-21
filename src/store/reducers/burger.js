f26f58ea63dfbb835a1751a2c08269ca3f5ca104
David Pal <paldavid2030@gmail.com>
2020-02-21 11:35:29 +0300

    add redux reducer: burger.js

---
+++ src/store/reducers/burger.js
@@ -0,0 +1,22 @@
+const initialState = {
+  ingredients: {
+    salad: 0,
+    cheese: 0,
+    bacon: 0,
+    meat: 0
+  },
+  finalPrice: 4
+}
+
+const reducer = (state = initialState, action) => {
+  const IngredientPriceList = {
+    salad: .5,
+    cheese: 1,
+    bacon: 1,
+    meat: 2,
+  }
+
+  return state;
+}
+
+export default reducer;
