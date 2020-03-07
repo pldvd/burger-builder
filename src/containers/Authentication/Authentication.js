460350624a8320c12cbb6da7ee0e9144446b1383
David Pal <paldavid2030@gmail.com>
2020-03-07 07:29:04 +0300

    create Authentication component

---
+++ src/containers/Authentication/Authentication.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Authentication extends Component {
+
+  render() {
+
+    return (
+      <div>Hello from Auth.</div>
+    )
+  }
+}
+
+export default Authentication;
