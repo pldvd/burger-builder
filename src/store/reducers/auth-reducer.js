da4a08bcf3e4fc09112706c27797fe44f8b40139
David Pal <paldavid2030@gmail.com>
2020-03-20 08:44:47 +0300

    auth reducer: define initial state

--- src/store/reducers/auth-reducer.js
+++ src/store/reducers/auth-reducer.js
@@ -1,7 +1,10 @@
 import {AUTH_START, AUTH_FAIL, AUTH_SUCCESS} from '../actions/actionTypes';
 
 const initialState = {
-
+  token: null,
+  userId: null,
+  loading: false,
+  error: null
 }
 
 const authReducer = (state = initialState, action) => {
