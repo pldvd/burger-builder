8170aa14e37db34fdd0d3dd1925ffcbdd0e4d9db
David Pal <paldavid2030@gmail.com>
2020-04-06 18:24:54 +0300

    Navbar displays log-out or log-in based on auth state

--- src/components/Toolbar/Navigation/Navigation.tsx
+++ src/components/Toolbar/Navigation/Navigation.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import styles from './Navigation.module.scss';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { useSelector} from 'react-redux';
 
 interface NavigationProps {
   type: string
 }
 
-const Navigation: React.FC<NavigationProps> = (props) => (
-  <ul className={[styles.Navigation, styles[props.type]].join(' ')}>
-    <NavLink to='/hello' activeClassName={styles.activeLink}>Hello</NavLink>
-    <NavLink to='/orders' activeClassName={styles.activeLink}>Orders</NavLink>
-    <NavLink to='/auth' activeClassName={styles.activeLink}>Log-in</NavLink>
-  </ul>
-);
+const Navigation: React.FC<NavigationProps> = (props) => {
+  const isAuthenticated = useSelector((state:any) => !!state.auth.token);
+
+  return (
+    <ul className={[styles.Navigation, styles[props.type]].join(' ')}>
+      <NavLink to='/hello' activeClassName={styles.activeLink}>Hello</NavLink>
+      <NavLink to='/orders' activeClassName={styles.activeLink}>Orders</NavLink>
+      { isAuthenticated
+        ? <NavLink to='/logout' activeClassName={styles.activeLink}>Log-out</NavLink>
+        : <NavLink to='/auth' activeClassName={styles.activeLink}>Log-in</NavLink>
+      }
+    </ul>
+  )
+};
 
 export default Navigation;
