448b984defa49d657f1061fcd7f4ec5f6f31110a
David Pal <paldavid2030@gmail.com>
2020-04-15 08:29:16 +0300

    Add more test cases

--- src/components/Toolbar/Navigation/Navigation.test.js
+++ src/components/Toolbar/Navigation/Navigation.test.js
@@ -3,27 +3,45 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Navigation from './Navigation';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 jest.mock('react-redux', () => {
- return {
-   useSelector: jest.fn()
+  return {
+    useSelector: jest.fn()
   }
 })
 
-describe("MySearchComponent", () => {
-//   beforeEach(() => {
-//     useSelector.mockImplementation((callback) => {
-//       return callback(mockAppState);
-//     });
-//   });
-//   afterEach(() => {
-//     useSelector.mockClear();
-//   });
+describe("NavComponent", () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((callback) => {
+      return callback({
+        auth: {
+          token: null
+        }
+      });
+    });
+  });
 
-  test('Navigation ul has two children if user is not logged in', () => {
-    const {queryByText } = render(<Router><Navigation /></Router>);
+  afterEach(() => {
+    useSelector.mockClear();
+  });
+
+  it('should not contain log-out if user is not authenticated', () => {
+    const { queryByText } = render(<Router><Navigation /></Router>);
     const logoutText = queryByText(/log-out/i)
     expect(logoutText).toBe(null);
   })
- })
+
+  it('should contain log-out if user is authenticated', () => {
+    const autheticatedState = {
+      auth: {
+        token: "adsfmasdökfksaöfdlmadsflösakdfö"
+      }
+    }
+
+    useSelector.mockImplementation(callback => callback(autheticatedState))
+    const { getAllByText } = render(<Router><Navigation /></Router>);
+    const logoutTextArr = getAllByText(/log-out/i)
+    expect(logoutTextArr).toHaveLength(1);
+  })
+})
